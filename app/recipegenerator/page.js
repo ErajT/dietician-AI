a1729704063066ea4364d1491d2d51d8a8a3c882
Aleeza Hussain <aleezahussain237@gmail.com>
2024-09-12 16:55:41 +0500

    recipe_generator home page

---
+++ app/recipegenerator/page.js
@@ -0,0 +1,27 @@
+"use client"; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import SearchBar from '../components/searchbar';
+import '../styling/recipegenerator.css'; 
+import CarouselRecipe from '../components/carousal'
+
+
+export default function RecipeGeneratorPage() {
+  const handleSearch = (query) => {
+    console.log('Search Query:', query);
+    // Add logic here to handle the search
+  };
+
+  return (
+    <div className="recipe-background">
+    <div className="overlay">
+      <h1 className='recipe-generator'>Recipe Generator</h1>
+      <h3 className='recipe-subheading'>Quick & Easy Recipe</h3>
+      <SearchBar onSearch={handleSearch} />
+    </div>
+  </div>
+  );
+}
+
+
+
